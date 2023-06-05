80f800eadc746ba465d93abcdc72694ea42d2f41
scolley <scolley.huang@kkday.com>
2023-06-05 21:49:25 +0800

    implement walking skeleton

---
+++ LegendsOfTheThreeKingdoms/src/main/java/com/waterball/LegendsOfTheThreeKingdoms/LegendsOfTheThreeKingdomsApplication.java
@@ -0,0 +1,13 @@
+package com.waterball.LegendsOfTheThreeKingdoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendsOfTheThreeKingdomsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LegendsOfTheThreeKingdomsApplication.class, args);
+
+	}
+
+}
