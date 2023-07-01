5b8f47a9e4fca75b725fd88d784623277ba3c4c4
scolley <scolley.huang@kkday.com>
2023-07-02 00:21:12 +0800

    refactor GameController

--- LegendsOfTheThreeKingdoms/src/main/java/com/waterball/LegendsOfTheThreeKingdoms/controller/GameController.java
+++ LegendsOfTheThreeKingdoms/src/main/java/com/waterball/LegendsOfTheThreeKingdoms/controller/GameController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-public class  GameController {
+public class GameController {
 
     private final GameService gameService;
 
