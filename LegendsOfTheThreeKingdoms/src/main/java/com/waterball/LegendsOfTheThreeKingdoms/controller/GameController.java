0c83c107a78ec4bf9ef0ed6c51ea6593b388ca81
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2024-01-16 23:13:57 +0800

    Add CORS annotation on GameController

--- LegendsOfTheThreeKingdoms/src/main/java/com/waterball/LegendsOfTheThreeKingdoms/controller/GameController.java
+++ LegendsOfTheThreeKingdoms/src/main/java/com/waterball/LegendsOfTheThreeKingdoms/controller/GameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class GameController {
 
     private final GameService gameService;
