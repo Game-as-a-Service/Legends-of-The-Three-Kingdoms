00e4c7e98e1f0f0bf81765ab8cff466f1c936130
Scolley <scolley31@gmail.com>
2024-09-18 01:13:28 +0800

    add monogodb setup

---
+++ LegendsOfTheThreeKingdoms/spring/src/main/java/com/gaas/threeKingdoms/QuickStart.java
@@ -0,0 +1,25 @@
+package com.gaas.threeKingdoms;
+
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class QuickStart {
+    public static void main( String[] args ) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://allen87295:LR2p62CirvPswcTU@legends-of-the-three-ki.ucjx7.mongodb.net/?retryWrites=true&w=majority&appName=legends-of-the-three-kingdoms";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
